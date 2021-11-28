8a0d1894a9eaf984c96005ca28cfc3b18cbe1702
pram212 <pramono6236@gmail.com>
2021-11-28 20:07:29 +0700

    templating blade

---
+++ pramono/perpus-app/resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+
+Dashboard
+
+@stop
+
+@section('content')
+
+Welcome to this beautiful admin panel.
+
+@stop
+
+@section('css')
+
+@stop
+
+@section('js')
+
+@stop
