d8bc939e138143d16a8f1b83c9770b59434c3559
pram212 <pramono6236@gmail.com>
2021-11-29 12:40:54 +0700

    perbaikan migrasi

--- pramono/perpus-app/database/migrations/2014_10_12_000001_create_users_table.php
+++ pramono/perpus-app/database/migrations/2014_10_12_000001_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('member_id')->nullable()->constrained();
             $table->rememberToken();
             $table->timestamps();
         });
