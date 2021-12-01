05a39025535e7c8858ac79589a5df2956aa5039b
eldynmohammad <eldyn.mohammad@gmail.com>
2021-12-01 12:44:08 +0700

    fixing bugs and create query builder

--- eldyn/library/database/seeders/DatabaseSeeder.php
+++ eldyn/library/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 		$this->call([
+			MemberSeeder::class,
 			AdminSeeder::class,
 			AuthorSeeder::class,
 			PublisherSeeder::class,
