147585d9888984d4428c8fd654c129d9c1d79f5a
muhamadhaspin <muhamadhaspin@gmail.com>
2021-12-05 07:54:54 +0700

    fixing catalog table title's typo

--- muhamad/library/resources/views/admin/catalog/index.blade.php
+++ muhamad/library/resources/views/admin/catalog/index.blade.php
@@ -7,7 +7,7 @@
     <div class="col-12">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Catalog;s Data</h3>
+                <h3 class="card-title">Catalog's Data</h3>
 
                 <div class="card-tools">
                     <div class="input-group input-group-sm" style="width: 150px;">
