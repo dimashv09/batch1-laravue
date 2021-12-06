dcb949ac5aa893ad9d7402503126c10f4ba80852
muhamadhaspin <muhamadhaspin@gmail.com>
2021-12-07 04:39:46 +0700

    Make a call method to invoke All Seeder in DatabaseSeeder

--- muhamad/library/database/seeders/DatabaseSeeder.php
+++ muhamad/library/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            MemberSeeder::class,
+            CatalogSeeder::class,
+            PublisherSeeder::class,
+            AuthorSeeder::class,
+            BookSeeder::class,
+        ]);
     }
 }
