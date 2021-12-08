6907f44594ada09eff34db4a192d130b43126fff
pram212 <pramono6236@gmail.com>
2021-12-08 21:25:04 +0700

    menghilangkan alert yang tidak perlu setiap memuat halaman

--- pramono/perpus-app/resources/views/layouts/app.blade.php
+++ pramono/perpus-app/resources/views/layouts/app.blade.php
@@ -84,8 +84,6 @@
     <script src="{{asset('vendor/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <!-- AdminLTE App -->
     <script src="{{asset('vendor/dist/js/adminlte.min.js')}}"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{asset('vendor/dist/js/demo.js')}}"></script>
 
     @stack('script')
 </body>
