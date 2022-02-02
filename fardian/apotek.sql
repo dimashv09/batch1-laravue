aeb1ecfea0d2668be3a8951158551c82895f789b
fardianazh <fardianazh20@gmail.com>
2022-02-02 19:30:37 +0700

    pembaharuan database

---
+++ fardian/apotek.sql
@@ -0,0 +1,356 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 02, 2022 at 01:27 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `apotek`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id` int(11) DEFAULT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL,
+  `total_obat` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id`, `id_transaksi`, `id_obat`, `total_obat`) VALUES
+(0, 1, 1, '5 buah'),
+(0, 2, 2, '2 buah');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dokter`
+--
+
+CREATE TABLE `dokter` (
+  `id` int(11) NOT NULL,
+  `nama_dokter` varchar(30) NOT NULL,
+  `spesialis_dokter` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dokter`
+--
+
+INSERT INTO `dokter` (`id`, `nama_dokter`, `spesialis_dokter`) VALUES
+(1, 'ALOHA SIMBOLON', 'KULIT'),
+(2, 'MEYSURI INDINAHUY', 'KELAMIN'),
+(3, 'FARDIAN', 'UMUM'),
+(4, 'ALEXANDRIA', 'JANTUNG'),
+(5, 'OPRA', 'PARU-PARU'),
+(6, 'KALEO', 'ORGANDALAM'),
+(7, 'UNTUNG', 'THT'),
+(8, 'LEOPARD', 'LAMBUNG'),
+(9, 'JAJANG MULYANA', 'TULANG'),
+(10, 'CECEP', 'KECANTIKAN');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis_obat`
+--
+
+CREATE TABLE `jenis_obat` (
+  `id` int(11) NOT NULL,
+  `tipe_jenis_obat` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `jenis_obat`
+--
+
+INSERT INTO `jenis_obat` (`id`, `tipe_jenis_obat`) VALUES
+(5, 'KERAS'),
+(6, 'RINGAN');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+
+CREATE TABLE `obat` (
+  `id` int(11) NOT NULL,
+  `nama_obat` varchar(20) NOT NULL,
+  `komposisi` varchar(80) NOT NULL,
+  `deskripsi_obat` text DEFAULT NULL,
+  `manufaktur` varchar(30) NOT NULL,
+  `id_perusahaan` int(11) NOT NULL,
+  `id_jenis_obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `obat`
+--
+
+INSERT INTO `obat` (`id`, `nama_obat`, `komposisi`, `deskripsi_obat`, `manufaktur`, `id_perusahaan`, `id_jenis_obat`) VALUES
+(1, 'Decolgenz', 'simili simili simili', 'simili simili simili', 'KALBE', 1, 5),
+(2, 'Inzana', 'simili simili', NULL, 'LABORA', 2, 6),
+(4, 'Insana', 'dududududu', 'bal bal bal', 'KALBE', 1, 6),
+(5, 'Hexagrip', 'dudududuud', NULL, 'KALBE', 1, 6),
+(6, 'Poldanmig', 'dudududuudududu', NULL, 'LABORA', 2, 5),
+(7, 'Fluzen', 'okokokookokoko', NULL, 'LABORA', 2, 6),
+(8, 'Inhamzo', 'kokokokokokokokok', NULL, 'KALBE', 1, 5),
+(9, 'Kalpotek', 'kikikikikikik', NULL, 'KALBE', 1, 5),
+(10, 'Lampionezes', 'kikikikikiikk', NULL, 'LABORA', 2, 5),
+(11, 'Inhumanizion', 'kikikikikikikik', NULL, 'KALBE', 1, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id` int(11) NOT NULL,
+  `nama_pasien` varchar(30) NOT NULL,
+  `alamat` varchar(40) NOT NULL,
+  `no_telepon` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`id`, `nama_pasien`, `alamat`, `no_telepon`) VALUES
+(1, 'INDAH PERMATA', 'JL. INDAH KESUKMAAN, BANDUNG', '0812732623'),
+(2, 'ALAMNA ORBANI', 'JL. KINTAMANI, JAKARTA', '0823929301'),
+(3, 'REZA', 'JL. PEGANGSAAN', '022-381912'),
+(4, 'BUKAN', 'JL. TIMUR RAYA', '022-3109012'),
+(5, 'UNTUNG GINTING', 'JL. INDAH SARI', '022-39212'),
+(6, 'KIKI', 'JL. GUNTUR', '0817322213'),
+(7, 'FATMAWATI', 'JL. ALAM RAYA', '081293022'),
+(8, 'RIRIN', 'JL. GUGUR BULAN', '021-3912831'),
+(9, 'GUMILAR', 'JL. VENUS', '023-392912'),
+(10, 'PERMATA SARI', 'JL. SATURNUS', '0838912831');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `perusahaan`
+--
+
+CREATE TABLE `perusahaan` (
+  `id` int(11) NOT NULL,
+  `nama_perusahaan` varchar(30) NOT NULL,
+  `alamat_perusahaan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `perusahaan`
+--
+
+INSERT INTO `perusahaan` (`id`, `nama_perusahaan`, `alamat_perusahaan`) VALUES
+(1, 'KALBE', 'Jl. Jakarta Timur'),
+(2, 'LABORA', 'Jl. Bandung Utara');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resep`
+--
+
+CREATE TABLE `resep` (
+  `id` int(11) NOT NULL,
+  `tanggal_resep` date NOT NULL,
+  `nama_obat` varchar(20) NOT NULL,
+  `nama_dokter` varchar(30) NOT NULL,
+  `nama_pasien` varchar(30) NOT NULL,
+  `id_dokter` int(11) NOT NULL,
+  `id_pasien` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `resep`
+--
+
+INSERT INTO `resep` (`id`, `tanggal_resep`, `nama_obat`, `nama_dokter`, `nama_pasien`, `id_dokter`, `id_pasien`) VALUES
+(1, '2022-02-17', 'Decolgen', 'ALOHA SIMBOLON', 'ALAMNA ORBANI', 1, 2),
+(2, '2022-02-26', 'Inzana', 'MEYSURI INDINAHUY', 'INDAH PERMATA', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `tanggal_beli` date NOT NULL,
+  `total_harga_obat` varchar(10) NOT NULL,
+  `id_resep` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `tanggal_beli`, `total_harga_obat`, `id_resep`) VALUES
+(1, '2022-02-02', '300.000', 1),
+(2, '2022-02-25', '500.000', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD KEY `fk_id_obat` (`id_obat`),
+  ADD KEY `fk_id_transaksi` (`id_transaksi`);
+
+--
+-- Indexes for table `dokter`
+--
+ALTER TABLE `dokter`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jenis_obat`
+--
+ALTER TABLE `jenis_obat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_perusahaan` (`id_perusahaan`),
+  ADD KEY `fk_jenis` (`id_jenis_obat`);
+
+--
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `perusahaan`
+--
+ALTER TABLE `perusahaan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `resep`
+--
+ALTER TABLE `resep`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_iddokter` (`id_dokter`),
+  ADD KEY `fk_idpasien` (`id_pasien`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_resep` (`id_resep`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dokter`
+--
+ALTER TABLE `dokter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `jenis_obat`
+--
+ALTER TABLE `jenis_obat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `perusahaan`
+--
+ALTER TABLE `perusahaan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `resep`
+--
+ALTER TABLE `resep`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `fk_id_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`),
+  ADD CONSTRAINT `fk_id_transaksi` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id`);
+
+--
+-- Constraints for table `obat`
+--
+ALTER TABLE `obat`
+  ADD CONSTRAINT `fk_jenis` FOREIGN KEY (`id_jenis_obat`) REFERENCES `jenis_obat` (`id`),
+  ADD CONSTRAINT `fk_perusahaan` FOREIGN KEY (`id_perusahaan`) REFERENCES `perusahaan` (`id`);
+
+--
+-- Constraints for table `resep`
+--
+ALTER TABLE `resep`
+  ADD CONSTRAINT `fk_iddokter` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id`),
+  ADD CONSTRAINT `fk_idpasien` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `fk_resep` FOREIGN KEY (`id_resep`) REFERENCES `resep` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
