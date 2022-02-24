99f97d9838dc1cf504e36fc36df00ce2d36b433a
fardianazh <fardianazh20@gmail.com>
2022-02-24 22:16:54 +0700

    compact data

--- fardian/library/resources/views/admin/catalog/index.blade.php
+++ fardian/library/resources/views/admin/catalog/index.blade.php
@@ -2,56 +2,31 @@
 @section ('header', 'Catalog')
 
 @section('content')
-<table class="table table-bordered">
+<div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Data Catalog</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table class="table table-bordered">
                   <thead>
                     <tr>
                       <th style="width: 10px">#</th>
-                      <th>Task</th>
-                      <th>Progress</th>
-                      <th style="width: 40px">Label</th>
+                      <th class ="text-center">Name</th>
+                      <th class ="text-center">Total Books</th>
+                      <th class ="text-center">Created At</th>
                     </tr>
                   </thead>
                   <tbody>
+                    @foreach($catalogs as $key => $catalog)
                     <tr>
-                      <td>1.</td>
-                      <td>Update software</td>
-                      <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-danger">55%</span></td>
+                      <td>{{ $key+1 }}</td>
+                      <td>{{ $catalog->name }}</td>
+                      <td class="text-center">{{ count($catalog->books) }}</td>
+                      <td class="text-center"> {{ date('d M Y', strtotime($catalog->created_at)) }}</td>
                     </tr>
-                    <tr>
-                      <td>2.</td>
-                      <td>Clean database</td>
-                      <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar bg-warning" style="width: 70%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-warning">70%</span></td>
-                    </tr>
-                    <tr>
-                      <td>3.</td>
-                      <td>Cron job running</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-primary" style="width: 30%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-primary">30%</span></td>
-                    </tr>
-                    <tr>
-                      <td>4.</td>
-                      <td>Fix and squish bugs</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-success" style="width: 90%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-success">90%</span></td>
-                    </tr>
+                    @endforeach
                   </tbody>
                 </table>
+              </div>
 @endsection
