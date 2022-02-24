8dda81df72d680fdd03e5e77c2668301d6f71481
sasangka lambang k <94464357+salak96@users.noreply.github.com>
2022-02-24 21:35:22 +0700

    Grafik peminjaman Belum tampil

--- sasangka/libray2/resources/views/home.blade.php
+++ sasangka/libray2/resources/views/home.blade.php
@@ -59,12 +59,10 @@
 </div>
 <section class="content">
     <div class="row">
-        {{-- Doughnut --}}
-
     </div>
     <!-- DONUT CHART -->
-    <div class="card card-success">
-        <div class="card-header">
+    <div class="card card-success" class="btn btn-secondary">
+        <div class="card-header accent-gray-dark">
             <h3 class="card-title">Grafik Penerbit</h3>
 
             <div class="card-tools">
@@ -80,136 +78,114 @@
             <canvas id="donutChart"
                 style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
         </div>
-        <!-- /.card-body -->
     </div>
-    <!-- /.card -->
-
-    <!-- PIE CHART -->
+    <!--barChart -->
     <div class="card card-warning">
-        <div class="card-header">
-            <h3 class="card-title">Grafik Peminjaman</h3>
 
-            <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                </button>
+        <div class="card card-success">
+            <div class="card-header">
+                <h3 class="card-title">Grafik Peminjaman</h3>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                        <i class="fas fa-times"></i>
+                    </button>
+                </div>
             </div>
-        </div>
-        <div class="card-body">
-            <canvas id="pieChart"
-                style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-        </div>
-        <!-- /.card-body -->
-    </div>
-    <!-- /.card -->
-    <div class="card-body" style="display: block;">
-        <div class="chartjs-size-monitor">
-            <div class="chartjs-size-monitor-expand">
-                <div class=""></div>
+            <div class="card-body">
+                <div class="chart">
+                    <canvas id="barChart"
+                        style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                </div>
             </div>
-            <div class="chartjs-size-monitor-shrink">
-                <div class=""></div>
-            </div>
+
         </div>
-        <canvas id="pieChart"
-            style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 951px;"
-            width="792" height="208" class="chartjs-render-monitor"></canvas>
-    </div>
-    @endsection
-    <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/chart.js/Chart.min.js') }}"></script>
-    <script text="text/javascript">
-        @section('js') <
-            !--ChartJS-- >
-            <
-            script src = "{{ asset('assets/plugins/chart.js/Chart.min.js') }}" >
 
-    </script>
+        @endsection
+        <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/plugins/chart.js/Chart.min.js') }}"></script>
+        <script text="text/javascript">
+            @section('js')
 
-    <script>
-        // Data Donut Chart
-        var data_donut = '{!! json_encode($data_donut) !!}'
-        var label_donut = '{!! json_encode($label_donut) !!}'
-        // Data Pie Chart
-        var dataPie = '{!! json_encode($data_pie) !!}'
-        var labelPie = '{!! json_encode($label_pie) !!}'
-        // Data Bar Chart
-        var dataBar = '{!! json_encode($data_bar) !!}'
+        </script>
 
+        <script>
+            // Data Donut Chart
+            var data_donut = '{!! json_encode($data_donut) !!}';
+            var label_donut = '{!! json_encode($label_donut) !!}';
+            // Data Pie Chart
+            var dataPie = '{!! json_encode($data_pie) !!}';
+            var labelPie = '{!! json_encode($label_pie) !!}';
+            // Data Bar Chart
+            var data_bar = '{!! json_encode($data_bar) !!}';
 
+            $(function () {
+                //- DONUT CHART -
+                //-------------
+                // Get context with jQuery - using jQuery's .get() method.
+                var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+                var donutData = {
+                    labels: JSON.parse(label_donut),
+                    datasets: [{
+                        data: JSON.parse(data_donut),
+                        backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc',
+                            '#d2d6de'
+                        ],
+                    }]
+                }
+                var donutOptions = {
+                    maintainAspectRatio: false,
+                    responsive: true,
+                }
+                //Create pie or douhnut chart
+                // You can switch between pie and douhnut using the method below.
+                new Chart(donutChartCanvas, {
+                    type: 'doughnut',
+                    data: donutData,
+                    options: donutOptions
+                })
 
-        $(function () {
-            //-------------
-            //- DONUT CHART -
-            //-------------
-            // Get context with jQuery - using jQuery's .get() method.
-            var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
-            var donutData = {
-                labels: JSON.parse (label_donut),
-                datasets: [
-                    {
-                    data:JSON.parse (data_donut),
-                    backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc',
-                        '#d2d6de'
+                //- PIE CHART -
+                //-------------
+                // Get context with jQuery - using jQuery's .get() method.
+                var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+                var pieData = donutData;
+                var pieOptions = {
+                    maintainAspectRatio: false,
+                    responsive: true,
+                }
+                //Create pie or douhnut chart
+                // You can switch between pie and douhnut using the method below.
+                new Chart(pieChartCanvas, {
+                    type: 'pie',
+                    data: pieData,
+                    options: pieOptions
+                })
+
+                //- BAR CHART -
+                var barChartData = {
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+                        'September', 'October', 'November', 'December'
                     ],
-                }]
-            }
-            var donutOptions = {
-                maintainAspectRatio: false,
-                responsive: true,
-            }
-            //Create pie or douhnut chart
-            // You can switch between pie and douhnut using the method below.
-            new Chart(donutChartCanvas, {
-                type: 'doughnut',
-                data: donutData,
-                options: donutOptions
+                    datasets: JSON.parse(data_bar)
+                }
+                var barChartCanvas = $('#barChart').get(0).getContext('2d')
+                var barChartData = $.extend(true, {}, barChartData)
+                var barChartOptions = {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    datasetFill: false
+                }
+                new Chart(barChartCanvas, {
+                    type: 'bar',
+                    data: barChartData,
+                    options: barChartOptions
+                })
             })
 
-            //- PIE CHART -
-            //-------------
-            // Get context with jQuery - using jQuery's .get() method.
-            var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
-            var pieData = donutData;
-            var pieOptions = {
-                maintainAspectRatio: false,
-                responsive: true,
-            }
-            //Create pie or douhnut chart
-            // You can switch between pie and douhnut using the method below.
-            new Chart(pieChartCanvas, {
-                type: 'pie',
-                data: pieData,
-                options: pieOptions
-            })
+        </script>
+        </body>
 
-            //-------------
-            //- BAR CHART -
-            //-------------
-            var barChartCanvas = $('#barChart').get(0).getContext('2d')
-            var barChartData = $.extend(true, {}, areaChartData)
-            var temp0 = areaChartData.datasets[0]
-            var temp1 = areaChartData.datasets[1]
-            barChartData.datasets[0] = temp1
-            barChartData.datasets[1] = temp0
-
-            var barChartOptions = {
-                responsive: true,
-                maintainAspectRatio: false,
-                datasetFill: false
-            }
-
-            new Chart(barChartCanvas, {
-                type: 'bar',
-                data: barChartData,
-                options: barChartOptions
-            })
-        })
-
-    </script>
-    </body>
-
-    </html>
+        </html>
