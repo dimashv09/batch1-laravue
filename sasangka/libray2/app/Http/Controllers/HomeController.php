3b19252a85e028f37a478be73156409eb56b8a12
sasangka lambang k <94464357+salak96@users.noreply.github.com>
2022-02-24 14:30:36 +0700

    grafic

--- sasangka/libray2/app/Http/Controllers/HomeController.php
+++ sasangka/libray2/app/Http/Controllers/HomeController.php
@@ -30,6 +30,68 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+
+	public function dashboard() {
+		$total_members = Member::count();
+		$total_books = Book::count();
+		$total_transactions = Transaction::whereMonth('date_start', date('m'))->count();
+		$total_publishers = Publisher::count();
+
+
+		$data_donut = Book::select(DB::raw("COUNT(publisher_id) as total"))
+					->groupBy('publisher_id')
+					->orderBy('publisher_id', 'asc')
+					->pluck('total');
+
+		// return $data_donut;
+
+		$label_donut = Publisher::orderBy('publishers.id', 'asc')
+					->join('books', 'books.publisher_id', '=', 'publishers.id')
+					->groupBy('publishers.name')
+					->pluck('publishers.name');
+					// return $label_donut;
+
+		// Chart Pie
+		$data_pie = Book::select('price')
+					->groupBy('price')
+					->orderBy('title', 'asc')
+					->pluck('price');
+
+		// return $data_pie;
+
+		$label_pie = Book::orderBy('title', 'asc')
+					->groupBy('title')
+					->pluck('title');
+					// return $label_pie;
+
+		// Chart Bar
+		$label_bar = ['Peminjaman', 'Pengembalian'];
+		$data_bar = [];
+
+		foreach ($label_bar as $key => $value) {
+			$data_bar[$key]['label'] = $label_bar[$key];
+			$data_bar[$key]['backgroundColor'] = $key == 0 ? 'rgba(60, 141, 188, 0.9)' : 'rgba(210, 214, 222, 1)';
+			$data_month = [];
+
+			foreach (range(1, 12) as $month) {
+				if ($key == 0) {
+					$data_month[] = Transaction::select(DB::raw("COUNT(*) as total"))
+					->whereMonth('date_start', $month)
+					->first()
+					->total;
+				} else {
+					$data_month[] = Transaction::select(DB::raw("COUNT(*) as total"))
+					->whereMonth('date_end', $month)
+					->first()
+					->total;
+				}
+			}
+			$data_bar[$key]['data'] = $data_month;
+		}
+		// return $data_bar;
+		return view('home', compact('total_members', 'total_books', 'total_transactions', 'total_publishers', 'data_donut', 'label_donut', 'data_pie', 'label_pie', 'data_bar', 'label_bar'));
+	}
+
     public function index()
     {
         // $books = Book::with('publisher')->with('catalog')->with('author')->get();
--- sasangka/libray2/resources/views/home.blade.php
+++ sasangka/libray2/resources/views/home.blade.php
@@ -5,180 +5,197 @@
     <div class="col-lg-3 col-6">
         <div class="small-box bg-info">
             <div class="inner">
-                {{-- <h3>{{ $total_books }}</h3> --}}
+                <h3>{{ $total_books }}</h3>
                 <p>Total Buku</p>
             </div>
             <div class="icon">
                 <i class="fa fa-book"></i>
             </div>
-            {{-- <a href="{{  url('books') }}" class="small-box-footer">More Info <i class="fa fa-arrow-circle-right"></i></a> --}}
+            <a href="{{  url('books') }}" class="small-box-footer">More Info <i
+                class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
     <div class="col-lg-3 col-6">
         <div class="small-box bg-dark">
             <div class="inner">
-                {{-- <h3>{{ $total_members }}</h3> --}}
+                <h3>{{ $total_members }}</h3>
                 <p>Total Anggota</p>
             </div>
             <div class="icon">
                 <i class="fa fa-book"></i>
             </div>
-            {{-- <a href="{{  url('members') }}" class="small-box-footer">More Info <i class="fa fa-arrow-circle-right"></i></a> --}}
+            <a href="{{  url('members') }}" class="small-box-footer">More Info <i
+                class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
 
     <div class="col-lg-3 col-6">
         <div class="small-box bg-primary">
             <div class="inner">
-                {{-- <h3>{{ $total_publishers }}</h3> --}}
+                <h3>{{ $total_publishers }}</h3>
                 <p>Data Penerbit</p>
             </div>
             <div class="icon">
                 <i class="fa fa-book"></i>
             </div>
-            {{-- <a href="{{  url('publishers') }}" class="small-box-footer">More Info <i class="fa fa-arrow-circle-right"></i></a> --}}
+            <a href="{{  url('publishers') }}" class="small-box-footer">More Info <i
+                class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
     <div class="col-lg-3 col-6">
         <div class="small-box bg-danger">
             <div class="inner">
-                {{-- <h3>{{ $total_transactions }}</h3> --}}
+                <h3>{{ $total_transactions }}</h3>
                 <p>Data Peminjaman</p>
             </div>
             <div class="icon">
                 <i class="fa fa-book"></i>
             </div>
-            {{-- <a href="{{  url('transactions') }}" class="small-box-footer">More Info <i class="fa fa-arrow-circle-right"></i></a> --}}
-            </div>
+            <a href="{{  url('transactions') }}" class="small-box-footer">More Info <i
+                class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
 </div>
+</div>
 <section class="content">
     <div class="row">
-  {{-- Doughnut --}}
+        {{-- Doughnut --}}
 
-</div>
- <!-- DONUT CHART -->
-<div class="card card-success">
-  <div class="card-header">
-    <h3 class="card-title">Grafik Penerbit</h3>
-
-    <div class="card-tools">
-      <button type="button" class="btn btn-tool" data-card-widget="collapse">
-        <i class="fas fa-minus"></i>
-      </button>
-      <button type="button" class="btn btn-tool" data-card-widget="remove">
-        <i class="fas fa-times"></i>
-      </button>
     </div>
-  </div>
-  <div class="card-body">
-    <canvas id="donutChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-  </div>
-  <!-- /.card-body -->
-</div>
-<!-- /.card -->
+    <!-- DONUT CHART -->
+    <div class="card card-success">
+        <div class="card-header">
+            <h3 class="card-title">Grafik Penerbit</h3>
 
-<!-- PIE CHART -->
-<div class="card card-warning">
-  <div class="card-header">
-    <h3 class="card-title">Grafik Peminjaman</h3>
-
-    <div class="card-tools">
-      <button type="button" class="btn btn-tool" data-card-widget="collapse">
-        <i class="fas fa-minus"></i>
-      </button>
-      <button type="button" class="btn btn-tool" data-card-widget="remove">
-        <i class="fas fa-times"></i>
-      </button>
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+        </div>
+        <div class="card-body">
+            <canvas id="donutChart"
+                style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+        </div>
+        <!-- /.card-body -->
     </div>
-  </div>
-  <div class="card-body">
-    <canvas id="pieChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-  </div>
-  <!-- /.card-body -->
-</div>
-<!-- /.card -->
-<div class="card-body" style="display: block;"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
-                <canvas id="pieChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 951px;" width="792" height="208" class="chartjs-render-monitor"></canvas>
-              </div>
-@endsection
-<script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/chart.js/Chart.min.js') }}"></script>
-<script text="text/javascript">
+    <!-- /.card -->
 
-  $(function () {
-   //-------------
-    //- DONUT CHART -
-    //-------------
-    // Get context with jQuery - using jQuery's .get() method.
-    var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
-    var donutData        = {
-      labels: [
-          'Chrome',
-          'IE',
-          'FireFox',
-          'Safari',
-          'Opera',
-          'Navigator',
-      ],
-      datasets: [
-        {
-          data: [700,500,400,600,300,100],
-          backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
-        }
-      ]
-    }
-    var donutOptions     = {
-      maintainAspectRatio : false,
-      responsive : true,
-    }
-    //Create pie or douhnut chart
-    // You can switch between pie and douhnut using the method below.
-    new Chart(donutChartCanvas, {
-      type: 'doughnut',
-      data: donutData,
-      options: donutOptions
-    })
+    <!-- PIE CHART -->
+    <div class="card card-warning">
+        <div class="card-header">
+            <h3 class="card-title">Grafik Peminjaman</h3>
 
-    //- PIE CHART -
-    //-------------
-    // Get context with jQuery - using jQuery's .get() method.
-    var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
-    var pieData        = donutData;
-    var pieOptions     = {
-      maintainAspectRatio : false,
-      responsive : true,
-    }
-    //Create pie or douhnut chart
-    // You can switch between pie and douhnut using the method below.
-    new Chart(pieChartCanvas, {
-      type: 'pie',
-      data: pieData,
-      options: pieOptions
-    })
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+        </div>
+        <div class="card-body">
+            <canvas id="pieChart"
+                style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+    <div class="card-body" style="display: block;">
+        <div class="chartjs-size-monitor">
+            <div class="chartjs-size-monitor-expand">
+                <div class=""></div>
+            </div>
+            <div class="chartjs-size-monitor-shrink">
+                <div class=""></div>
+            </div>
+        </div>
+        <canvas id="pieChart"
+            style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 951px;"
+            width="792" height="208" class="chartjs-render-monitor"></canvas>
+    </div>
+    @endsection
+    <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/chart.js/Chart.min.js') }}"></script>
+    <script text="text/javascript">
+        $(function () {
+                    //-------------
+                    //- DONUT CHART -
+                    //-------------
+                    // Get context with jQuery - using jQuery's .get() method.
+                    var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+                    var donutData = {
+                        labels: [
+                            'Chrome',
+                            'IE',
+                            'FireFox',
+                            'Safari',
+                            'Opera',
+                            'Navigator',
+                        ],
+                        datasets: [{
+                            data: [700, 500, 400, 600, 300, 100],
+                            backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc',
+                                '#d2d6de'
+                            ],
+                        }]
+                    }
+                    var donutOptions = {
+                        maintainAspectRatio: false,
+                        responsive: true,
+                    }
+                    //Create pie or douhnut chart
+                    // You can switch between pie and douhnut using the method below.
+                    new Chart(donutChartCanvas, {
+                        type: 'doughnut',
+                        data: donutData,
+                        options: donutOptions
+                    })
 
-    //-------------
-    //- BAR CHART -
-    //-------------
-    var barChartCanvas = $('#barChart').get(0).getContext('2d')
-    var barChartData = $.extend(true, {}, areaChartData)
-    var temp0 = areaChartData.datasets[0]
-    var temp1 = areaChartData.datasets[1]
-    barChartData.datasets[0] = temp1
-    barChartData.datasets[1] = temp0
+                    //- PIE CHART -
+                    //-------------
+                    // Get context with jQuery - using jQuery's .get() method.
+                    var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+                    var pieData = donutData;
+                    var pieOptions = {
+                        maintainAspectRatio: false,
+                        responsive: true,
+                    }
+                    //Create pie or douhnut chart
+                    // You can switch between pie and douhnut using the method below.
+                    new Chart(pieChartCanvas, {
+                        type: 'pie',
+                        data: pieData,
+                        options: pieOptions
+                    })
 
-    var barChartOptions = {
-      responsive              : true,
-      maintainAspectRatio     : false,
-      datasetFill             : false
-    }
+                    //-------------
+                    //- BAR CHART -
+                    //-------------
+                    var barChartCanvas = $('#barChart').get(0).getContext('2d')
+                    var barChartData = $.extend(true, {}, areaChartData)
+                    var temp0 = areaChartData.datasets[0]
+                    var temp1 = areaChartData.datasets[1]
+                    barChartData.datasets[0] = temp1
+                    barChartData.datasets[1] = temp0
 
-    new Chart(barChartCanvas, {
-      type: 'bar',
-      data: barChartData,
-      options: barChartOptions
-    })
+                    var barChartOptions = {
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        datasetFill: false
+                    }
+
+                    new Chart(barChartCanvas, {
+                        type: 'bar',
+                        data: barChartData,
+                        options: barChartOptions
+                    })
+        })
 </script>
 </body>
+
 </html>
