6c6fafb0e7cf9b4fc6835dbba83c205c4842478f
Fajar Maulana <fajarrmaulanna@gmail.com>
2022-03-11 00:06:37 +0700

    create templating regis/login/home

--- fajar/library/resources/views/home.blade.php
+++ fajar/library/resources/views/home.blade.php
@@ -1,20 +1,12 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                <div class="card-body text-center">
+                    <h5>Welcome To Home Page</h5>
                 </div>
             </div>
         </div>
