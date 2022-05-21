f0c17840ebfaaa392899743fb3f03e798814f3c6
Fajar Maulana <fajarrmaulanna@gmail.com>
2022-05-21 20:03:59 +0700

    error crud book

--- fajar/library/resources/views/admin/book/index.blade.php
+++ fajar/library/resources/views/admin/book/index.blade.php
@@ -181,6 +181,7 @@
                     _this.book.ajax.reload();
                 });
             },
+            
             numberWithSpaces(x) {
                 return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
             }
