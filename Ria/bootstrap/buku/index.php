b6343f2828d070b64d93a69d5fb7b0337d3f7b62
Ria Fatimah <103774939+Riafatimah@users.noreply.github.com>
2022-05-27 21:50:50 +0700

    Update index.php

--- Ria/bootstrap/buku/index.php
+++ Ria/bootstrap/buku/index.php
@@ -11,9 +11,7 @@
 <html>
 <head>
 
-	<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
 	<title>Homepage</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
