f0a7df8e9650350885a6dc523c3514e0579d88e5
Ria Fatimah <103774939+Riafatimah@users.noreply.github.com>
2022-07-04 22:40:05 +0700

    Add files via upload

--- Ria/mylibrary/resources/views/layouts/admin.blade.php
+++ Ria/mylibrary/resources/views/layouts/admin.blade.php
@@ -145,10 +145,58 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
-            <a href="#"class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+            <a href="{{ url(('home')) }}"class="nav-link {{ request()->is('home') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-home"></i>
               <p>
                 Home
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url(('member')) }}"class="nav-link {{ request()->is('member') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                Member
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url(('catalog')) }}"class="nav-link {{ request()->is('catalog') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-list"></i>
+              <p>
+                Catalog
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url(('book')) }}"class="nav-link {{ request()->is('book') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-book"></i>
+              <p>
+                Book
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url(('author')) }}"class="nav-link {{ request()->is('autor') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-marker"></i>
+              <p>
+                Author
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url(('publisher')) }}"class="nav-link {{ request()->is('publisher') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-briefcase"></i>
+              <p>
+                Publisher
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url(('transaction')) }}"class="nav-link {{ request()->is('transaction') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-sitemap"></i>
+              <p>
+                Transaction
               </p>
             </a>
           </li>
@@ -165,7 +213,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-              <h1 class="m-0">Dashboard</h1>
+              <h1 class="m-0">@yield('header')</h1>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
