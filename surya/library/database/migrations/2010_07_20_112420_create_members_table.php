5492fc3beb2e3b971d2002415e9819ee787683d9
yhaae01 <suryaintpermana@gmail.com>
2022-07-20 18:43:14 +0700

    Tugas Migration and Model

---
+++ surya/library/database/migrations/2010_07_20_112420_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 65);
+            $table->char('gender', 1);
+            $table->char('phone', 13);
+            $table->text('address');
+            $table->string('email', 65);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
---
+++ surya/library/database/migrations/2022_07_20_112514_create_publishers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePublishersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publishers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 65);
+            $table->string('email', 65);
+            $table->char('phone', 13);
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publishers');
+    }
+}
