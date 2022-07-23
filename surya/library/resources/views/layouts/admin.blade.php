2d53edc4abcb384f338ff47b881c11298ee5de58
yhaae01 <suryaintpermana@gmail.com>
2022-07-23 11:15:12 +0700

    Change footer copyright

--- surya/library/resources/views/layouts/admin.blade.php
+++ surya/library/resources/views/layouts/admin.blade.php
@@ -195,11 +195,8 @@
 
         <!-- /.content-wrapper -->
         <footer class="main-footer">
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+            <strong>Copyright &copy; 2022 <a href="https://github.com/yhaae01" target="_blank">Surya Intan Permana</a>.</strong>
             All rights reserved.
-            <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.1.0
-            </div>
         </footer>
 
         <!-- Control Sidebar -->
