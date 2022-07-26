1610abaee254354b82fd48ec0e2df1332763ddc0
jaenaladrikni33 <jaenal10032000@gmail.com>
2022-07-26 22:28:52 +0700

    apotek

--- jaenal/apotek.sql
+++ jaenal/apotek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Jul 2022 pada 08.48
+-- Waktu pembuatan: 26 Jul 2022 pada 17.04
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 8.1.6
 
@@ -24,22 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL,
+  `jumlah_obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id_transaksi`, `id_obat`, `jumlah_obat`) VALUES
+(2, 1997, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `obat`
 --
 
 CREATE TABLE `obat` (
   `id` int(11) NOT NULL,
   `nama_obat` varchar(32) NOT NULL,
-  `expired` date NOT NULL
+  `stok _obat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `obat`
 --
 
-INSERT INTO `obat` (`id`, `nama_obat`, `expired`) VALUES
-(1, 'depresi', '2028-06-21'),
-(2, 'baigon', '2022-07-30');
+INSERT INTO `obat` (`id`, `nama_obat`, `stok _obat`) VALUES
+(1997, 'sakit pinggang', 10),
+(1998, 'sakit perut', 12),
+(1999, 'pusing', 2);
 
 -- --------------------------------------------------------
 
@@ -48,23 +68,48 @@ INSERT INTO `obat` (`id`, `nama_obat`, `expired`) VALUES
 --
 
 CREATE TABLE `pembeli` (
+  `id_pembeli` int(11) NOT NULL,
   `nama_pembeli` varchar(32) NOT NULL,
-  `id_obat` int(11) NOT NULL,
-  `jenis_obat` text NOT NULL,
-  `tanggal_beli` date DEFAULT NULL
+  `umur` int(11) DEFAULT NULL,
+  `alamat` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `pembeli`
 --
 
-INSERT INTO `pembeli` (`nama_pembeli`, `id_obat`, `jenis_obat`, `tanggal_beli`) VALUES
-('radit', 1, 'loncat dari lantai 30', '2022-07-23'),
-('adul', 2, 'penghilang nyawa jika di minum berlebihan', '2022-07-29');
+INSERT INTO `pembeli` (`id_pembeli`, `nama_pembeli`, `umur`, `alamat`) VALUES
+(1997, 'adul', 12, 'tuauddsydaatjkkjatcbacbdah by08 dwgdai dwaou7bfsjm tbjkvs gkdbab dsbinds  fsbsatpus ');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_pembeli` int(11) NOT NULL,
+  `tanggal_transaksi` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_pembeli`, `tanggal_transaksi`) VALUES
+(2, 1997, '2022-07-06');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_obat` (`id_obat`);
 
 --
 -- Indeks untuk tabel `obat`
@@ -76,33 +121,53 @@ ALTER TABLE `obat`
 -- Indeks untuk tabel `pembeli`
 --
 ALTER TABLE `pembeli`
-  ADD PRIMARY KEY (`id_obat`);
+  ADD PRIMARY KEY (`id_pembeli`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_pembeli` (`id_pembeli`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT untuk tabel `obat`
+-- AUTO_INCREMENT untuk tabel `detail_transaksi`
 --
-ALTER TABLE `obat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `detail_transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `pembeli`
 --
 ALTER TABLE `pembeli`
-  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pembeli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1998;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Ketidakleluasaan untuk tabel `pembeli`
+-- Ketidakleluasaan untuk tabel `detail_transaksi`
 --
-ALTER TABLE `pembeli`
-  ADD CONSTRAINT `fk_pembeli` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`);
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`),
+  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
