31de560bc5c0808f45086b39dce1b7c93c699198
YutsuriHisekawa <tetsuyakumiko01@gmail.com>
2022-12-15 21:06:00 +0700

    create new folder

--- Fahrizal/Backup Data/Tugas_Php/CRUD/buku/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../connect.php");
-
-$isbn = $_GET['isbn'];
-
-$result = mysqli_query($mysqli, "DELETE FROM buku WHERE isbn='$isbn'");
-
-header("Location:index.php");
-?>
--- Fahrizal/Backup Data/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Fahrizal</title>
-</head>
-<body>
-    <h1> FAHRIZAL JULIAN ADAM</h1>
-</body>
-</html>
