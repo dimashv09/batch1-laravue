164cf964281515a58ded442b05a5ec925b1be57e
cepbasyir <cecepbasyir@gmail.com>
2023-07-14 20:45:40 +0700

    tugas php

---
+++ cecep/tugas_php/bangun_datar.php
@@ -0,0 +1,38 @@
+<!DOCTYPEhtml>
+<html>
+    <head>
+        <title><?php echo "bangun datar";?></title>
+    </head>
+    <body>
+        <?php
+    // Luas Persegi
+    // $c = Luas, $a = sisi, $b = sisi
+        $c = $a*$b;
+        echo ="$a*$b=$c";
+        echo ="<hr>";
+
+    // Luas Peregi Panjang
+    // $c = Luas, $a = Panjang, $b = Lebar 
+    $c = $a*$b;
+    echo ="$a*$b=$c";
+    echo ="<hr>";
+
+    // Luas Segitiga 
+    // $d = Luas, $c = 1/2, $a= aLas, $b = tinggi
+    $d = $c*$a*$b;
+    echo ="$c*$a*$b=$d";
+    echo ="<hr>";
+
+    // Jajar Genjang
+    // $c = Luas, $a = sisi alas, $b = tinggi
+    $c=$a*$b;
+    echo ="$a*$b=$c"
+    echo ="<hr>";
+
+    // Belah Ketupat
+    //  $c = Luas, $d = 1/2, $a = diagonal 1, $b = diagonal 2
+    $c = $d*$a*$b;
+    echo ="$d*$a*$b=$sc";
+
+    ?>
+    </body>
---
+++ cecep/tugas_php/bangun_ruang.php
@@ -0,0 +1,29 @@
+<!DOCTYPEhtml>
+<html>
+    <head>
+        <title><?php echo "bangun ruang";?></title>
+    </head>
+    <body>
+        <?php
+
+            // kubus
+            // $c = volume, $a = sisi, $b = sisi, $d = sisi
+            $c = $a*$b*$d;
+            echo ="$a*$b*$d=$c";
+            echo ="<hr>";
+
+            // Balok
+            // $c = volume, $a = panjang, $b = lebar, $d = tinggi
+            $c = $a*$b*$d;
+            echo ="$a*$b*$d=$c";
+            echo ="<hr>";
+
+            // Prisma
+            // $c = volume, $a = luas alas, $b = tinggi
+            $c = $a*$b;
+            echo ="$a*$b=$c";
+            echo ="<hr>";
+
+
+        ?>
+    </body>
