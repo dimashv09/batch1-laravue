78349e1caee1559599262275794dc626187b4f77
igaauliani <iga.aulianirahman@gmail.com>
2023-10-22 19:57:41 +0700

    update apotek

--- igaauliani/apotek.sql
+++ igaauliani/apotek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 21, 2023 at 04:46 PM
+-- Generation Time: Oct 22, 2023 at 02:53 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.3.33
 
@@ -24,13 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `berobat`
+--
+
+CREATE TABLE `berobat` (
+  `id` int(11) NOT NULL,
+  `id_pasien` int(11) NOT NULL,
+  `id_dokter` int(11) NOT NULL,
+  `tanggal_berobat` date NOT NULL,
+  `keluhan` varchar(64) NOT NULL,
+  `obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `obat`
 --
 
 CREATE TABLE `obat` (
   `id` int(11) NOT NULL,
   `nama_obat` varchar(64) NOT NULL,
-  `stock_obat` int(11) DEFAULT NULL,
+  `id_stock` int(11) DEFAULT NULL,
   `tanggal_kadaluarsa` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -38,8 +53,49 @@ CREATE TABLE `obat` (
 -- Dumping data for table `obat`
 --
 
-INSERT INTO `obat` (`id`, `nama_obat`, `stock_obat`, `tanggal_kadaluarsa`) VALUES
+INSERT INTO `obat` (`id`, `nama_obat`, `id_stock`, `tanggal_kadaluarsa`) VALUES
 (1, 'obat batuk', 5, '2023-10-24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id` int(11) NOT NULL,
+  `nama_pasien` varchar(64) NOT NULL,
+  `jenis_kelamin` varchar(32) NOT NULL,
+  `umur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id` int(11) NOT NULL,
+  `id_pasien` varchar(10) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `tanggal_transaksi` date NOT NULL,
+  `total_transaksi` bigint(10) NOT NULL,
+  `total_bayar` bigint(10) NOT NULL,
+  `total_kembalian` bigint(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `id` int(11) NOT NULL,
+  `stock_obat` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -48,7 +104,7 @@ INSERT INTO `obat` (`id`, `nama_obat`, `stock_obat`, `tanggal_kadaluarsa`) VALUE
 --
 
 CREATE TABLE `transaksi` (
-  `id_transaksi` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `id_obat` int(11) NOT NULL,
   `jumlah_transaksi` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -57,12 +113,20 @@ CREATE TABLE `transaksi` (
 -- Dumping data for table `transaksi`
 --
 
-INSERT INTO `transaksi` (`id_transaksi`, `id_obat`, `jumlah_transaksi`) VALUES
+INSERT INTO `transaksi` (`id`, `id_obat`, `jumlah_transaksi`) VALUES
 (1, 1, 2);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `berobat`
+--
+ALTER TABLE `berobat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_pasien` (`id_pasien`),
+  ADD KEY `obat` (`obat`);
 
 --
 -- Indexes for table `obat`
@@ -71,15 +135,42 @@ ALTER TABLE `obat`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_transaksi` (`id_transaksi`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `stock_obat` (`stock_obat`),
+  ADD KEY `harga` (`harga`);
+
+--
 -- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD PRIMARY KEY (`id_transaksi`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `fk_obat` (`id_obat`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `berobat`
+--
+ALTER TABLE `berobat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `obat`
@@ -88,14 +179,52 @@ ALTER TABLE `obat`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `berobat`
+--
+ALTER TABLE `berobat`
+  ADD CONSTRAINT `berobat_ibfk_1` FOREIGN KEY (`obat`) REFERENCES `obat` (`id`),
+  ADD CONSTRAINT `fk_pasien` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id`);
+
+--
+-- Constraints for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD CONSTRAINT `penjualan_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id`);
+
+--
+-- Constraints for table `stock`
+--
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`stock_obat`) REFERENCES `obat` (`id`),
+  ADD CONSTRAINT `stock_ibfk_2` FOREIGN KEY (`harga`) REFERENCES `penjualan` (`id`);
 
 --
 -- Constraints for table `transaksi`
