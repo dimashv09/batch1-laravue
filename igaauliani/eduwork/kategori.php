bd2c22516d751eef97756a830ad07080a26f6d65
igaauliani <iga.aulianirahman@gmail.com>
2023-11-15 12:17:09 +0700

    kategori mbi

---
+++ igaauliani/eduwork/kategori.php
@@ -0,0 +1,21 @@
+<?php
+$berat = 100;
+$tinggi =1.78;
+
+//hitung bmi
+$bmi=$berat/($tinggi*$tinggi);
+
+//menentukan kategori
+if ($bmi < 18.5){
+   $kategori= " kurus ";
+}elseif ($bmi >= 18.5 && $bmi < 24.9) {
+   $kategori="normal";
+} else {
+    $kategori= "gemuk";
+}
+
+//menampilkan
+echo "BMI ANDA:". number_format($bmi, 1) ."<br>";
+echo "kategori BMI:". $kategori;
+
+?>
